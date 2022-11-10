f59ed3913129264c8cc0741ab7c5b1e7d1979a00
Rohan Singh <rs7289579@gmail.com>
2022-11-10 19:50:14 +0530

    all endpoints tested

---
+++ models/dashboardSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    prod_name :{
+        type: String,
+        required : true
+    },
+    ip_address :{
+        type: String,
+        require: true
+    },
+    mac_address :{
+        type: String,
+        require: true
+    },
+    function :{
+        type: String,
+        require: true
+    },
+    version :{
+        type: Number,
+        require: true
+    },
+    last_updated :{
+        type: Date,
+        require: true
+    }
+})
+
+const Product = new mongoose.model('Dashboard',ProductSchema)
+
+module.exports = Product;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,18 +1,101 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
+const express = require("express");
+const app = express();
+const router = express.Router();
+const Products = require("../models/dashboardSchema");
 
-router.get('/',(req,res)=> {
-    res.send('Get Request')
-})
-router.post('/',(req,res)=> {
-    res.send('POST Request')
-})
-router.put('/',(req,res)=> {
-    res.send('PUT Request')
-})
-router.delete('/',(req,res)=> {
-    res.send('DELETE Request')
-})
+const {
+  getProducts,
+  getSingleDashboard,
+  addDashboard,
+  updateDashboard,
+  deleteDashboard,
+} = require("../models/dashboardSchema");
+
+// router.get('/',getDashboard)
+
+router.get("/", async (req, res) => {
+  try {
+    const response = await Products.find();
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  let id = req.params.id;
+  let member = await Products.find({ _id: id });
+  if (member.length > 0) {
+    res.send(member);
+  } else {
+    res.status(404).json({ message: "no product found :(" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const ins = new Products({
+      prod_name: req.body.prod_name,
+      ip_address: req.body.ip_address,
+      mac_address: req.body.mac_address,
+      function: req.body.function,
+      version: Number.parseFloat(req.body.version),
+      last_updated: req.body.last_updated,
+    });
+    const response = await ins.save();
+    res.send(response);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let id = req.params.id;
+  let response = await Products.find({ _id: id });
+  if (response.length > 0) {
+    if (
+      req.body.prod_name.length >= 3 &&
+      req.body.ip_address.length >= 3 &&
+      req.body.mac_address.length >= 3 &&
+      req.body.function.length >= 3
+    ) {
+      try {
+        const response = await Products.findByIdAndUpdate(
+          { _id: id },
+          {
+            prod_name: req.body.prod_name,
+            ip_address: req.body.ip_address,
+            mac_address: req.body.mac_address,
+            function: req.body.function,
+            version: Number.parseFloat(req.body.version),
+            last_updated: req.body.last_updated,
+          }
+        );
+        res.send(response);
+      } catch (error) {
+        res.status(404).send(error);
+      }
+    } else {
+      res.status(500).json({ message: "length is too sort..." });
+    }
+  } else {
+    res.status(404).json({ message: "no product found :(" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  let id = req.params.id;
+  let response = await Products.find({ _id: id });
+  if (response.length > 0) {
+    try {
+      const response = await Products.findByIdAndDelete({ _id: id });
+      res.send(response);
+    } catch (error) {
+      res.status(404).send(error);
+    }
+  } else {
+    res.status(404).json({ message: "No Product found!" });
+  }
+});
 
 module.exports = router;
