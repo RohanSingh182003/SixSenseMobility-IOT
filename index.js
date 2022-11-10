e173d84510e3141d4b7175aceee301e040cfee7d
Rohan Singh <rs7289579@gmail.com>
2022-11-10 18:35:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+let port = process.env.PORT || 4000
+
+app.listen(port,()=> console.log(`app is listening on port ${port}`))
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
